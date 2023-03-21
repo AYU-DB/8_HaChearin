877bc35826aacb7ed2b898a7ba7378f04f2f6bee
didiha <hcring@naver.com>
2023-03-21 12:53:45 +0900

    20230321

---
+++ practice4.py
@@ -0,0 +1,25 @@
+print(1 + 1)
+print(3 - 2)
+print(6 / 3) # 2
+
+print(2 ** 3) # 2의 3제곱
+print(5 % 3) # 5 나누기 3의 몫이 아닌! 나머지인 2
+print(5 // 3) # 5를 3으로 나눈 몫인 1
+print(5 / 3) # 1이 나오는 게 아닌 1.666...
+
+print(4 <= 7) # True
+print(4 >= 7) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(3 != 3) # False
+print(not(1 != 3)) # False
+
+print((3 > 0 ) and (3 < 5)) # True
+print((3 > 0) & (3 < 5) ) # True / && 아니니까 조심!
+
+print((3 > 0) or (3 < 5)) # True
+print((3 > 0) | (3 < 5)) #True / || 아니니까 조심! 22
+
+print(3 < 4 < 5) # True
+print(5 > 4 > 7) # Flase
