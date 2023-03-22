8a19aea4d1b4a0ba1a50c9598927b52f9aaae655
didiha <hcring@naver.com>
2023-03-22 19:11:52 +0900

    20230322

---
+++ chap03/3-2.py
@@ -0,0 +1,24 @@
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+
+number = 2 + 3 * 4 # 14
+print(number)
+
+number = number + 2 # 16
+print(number)
+
+# number = number + 2 >>> 이렇게도 가능
+number += 2 # 18
+print(number)
+
+number *= 2 # 36
+print(number)
+
+number /= 2 # 18.0
+print(number)
+
+number -= 2 # 16.0
+print(number)
+
+number %= 5 # 1.0
+print(number)
