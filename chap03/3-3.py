e48414bdf29e4f1d2e071f42bbd572320b3b4cbc
didiha <hcring@naver.com>
2023-03-23 15:08:27 +0900

    20230323

---
+++ chap03/3-3.py
@@ -0,0 +1,12 @@
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12)) # 12 | 더 큰 값 반환
+print(min(5, 12)) # 5 | 더 작은 값 반환
+print(round(3.14)) # 3 | 반올림
+print(round(4.99)) # 5
+
+# math 라이브러리에 있는 모든 걸 사용하겠다
+from math import *
+print(floor(4.99)) # 4 | 내림
+print(ceil(3.14)) # 4 | 올림
+print(sqrt(16)) # 4 | 제곱근
