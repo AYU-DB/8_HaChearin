10f66b9b0be3594a9a6c05e085bb6db135464407
didiha <hcring@naver.com>
2023-04-13 00:44:13 +0900

    20230413

---
+++ chap04/4-5.py
@@ -0,0 +1,22 @@
+# \n
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \'
+print("저는 '3학년'입니다.")
+print('저는 "3학년"입니다.')
+print("저는 \"3학년\"입니다.")
+print("저는 \'3학년\'입니다.")
+
+# \\
+print("/\\/\\ << 눈 웃음")
+
+# \r : 커서를 맨 앞으로
+# Pine 이 맨 앞자리에 출력이 됨
+# 결과 : Pine sky
+print("Blue sky\rDark")
+
+# \b : 백스페이스(한 글자 삭제)
+print("Bluee\b sky")
+
+# \t : 탭
+print("Blue\tsky")
