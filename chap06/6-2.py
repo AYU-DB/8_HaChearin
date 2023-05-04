706ed4ba312cc7aa1a2e9dae7bc6fa5319c86d61
didiha <hcring@naver.com>
2023-05-04 21:34:31 +0900

    20230504

--- chap06/6-2.py
+++ chap06/6-2.py
@@ -1,2 +1,6 @@
-for waiting_num in[0,1,2,3,4]:
+for waiting_num in range(1,6): 
     print("대기번호: {0}".format(waiting_num))
+
+customer = ["a", "b", "c"]
+for order in customer:
+    print("{0}님, 주문하신 음료 나왔습니다.".format(order))
