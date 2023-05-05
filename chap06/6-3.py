4fd07c3db2d37a0afcacd8cca4ac382d41ef8db6
didiha <hcring@naver.com>
2023-05-05 20:17:50 +0900

    20230505

---
+++ chap06/6-3.py
@@ -0,0 +1,18 @@
+# while
+customer = "a"
+index = 5
+while index >= 1:
+    print("{0}, 음료가 준비되었습니다. {1}번 남았습니다.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분되었습니다.")
+
+customer = "b"
+index = 1
+while True:
+    print("{0}, 커피가 준비되었습니다. 호출 {1}회".format(customer, index))
+    index += 1
+    if index == 5:
+        break
+        
+        
