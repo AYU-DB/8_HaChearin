c5d8deae6ae4aa9c943897e24de94e3329cace26
didiha <hcring@naver.com>
2023-05-05 20:24:34 +0900

    20230505

--- chap06/6-3.py
+++ chap06/6-3.py
@@ -15,5 +15,10 @@ while True:
     if index == 6:
         print("음료는 폐기처분되었습니다.")
         break
-        
-        
+
+customer = "c"
+someone = "Unknown"
+
+while someone != customer:
+    print("{0}, 음료가 준비되었습니다.".format(someone))
+    someone = input("이름이 어떻게 되세요? ")
