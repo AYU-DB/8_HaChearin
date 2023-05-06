e305eb426d953f1633f11925465b48fe637b610b
didiha <hcring@naver.com>
2023-05-06 20:24:07 +0900

    20230506

--- chap06/6-4.py
+++ chap06/6-4.py
@@ -1,5 +1,9 @@
-absent = [2,5]
+absent = [2,5] # 2번 5번은 결석
+noBook = [7] # 책이 없음
 for student in range(1, 11):
     if student in absent:
         continue
     print("{0}, 책을 읽어봐".format(student))
+    if student in noBook:
+        print("제가 {0}번 책을 깜빡하고 안 가져왔습니다.".format(student))
+        print("그럼 {0}번은 옆 사람 책 빌려서 읽어.".format(student))
