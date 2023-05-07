adcba5555ede31c423ba70ea741db64d13153b80
didiha <hcring@naver.com>
2023-05-07 19:46:49 +0900

    20230507

--- chap06/6-5.py
+++ chap06/6-5.py
@@ -1,4 +1,10 @@
-# 출석 번호 1, 2.. 일 때 101, 102로 출력
+# 출석 번호 1, 2.. 일 때 101, 102로 저장 후 출력
 student = [1,2,3,4]
 print(student)
 student = [i+100 for i in student]
+print(student)
+
+# 학생 이름을 길이로 변환
+student = ["프로그래머" "사회 선생님" "미용사" "간호사"]
+student = [len(i) for i in student]
+print(student)
