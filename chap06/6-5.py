fe7e966f55923419b889812a66f5603ede74b853
didiha <hcring@naver.com>
2023-05-07 19:50:43 +0900

    20230507

--- chap06/6-5.py
+++ chap06/6-5.py
@@ -4,12 +4,14 @@ print(student)
 student = [i+100 for i in student]
 print(student)
 
-# 이름을 길이로 변환(각 이름의 길이의 합)
-student = ["프로그래머" "사회선생님" "미용사" "간호사"]
+# 각 이름을 길이로 변환
+student = ["프로그래머", "사회선생님", "미용사", "간호사"]
+print(student)
 student = [len(i) for i in student]
 print(student)
 
 # 학생 이름을 대문자로 변환
-student = ["aaA" "bBb" "Ccc"]
+student = ["aaA", "bBb", "Ccc"]
+print(student)
 student = [i.upper() for i in student]
 print(student)
