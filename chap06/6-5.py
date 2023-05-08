35c374529978eaf1dc797db9ff850f44e91dcc75
didiha <hcring@naver.com>
2023-05-08 21:16:49 +0900

    20230508

--- chap06/6-5.py
+++ chap06/6-5.py
@@ -1,11 +1,11 @@
 # 출석 번호 1, 2.. 일 때 101, 102로 저장 후 출력
 student = [1,2,3,4]
 print(student)
-student = [i+100 for i in student]
+student = [i + 100 for i in student]
 print(student)
 
 # 각 이름을 길이로 변환
-student = ["프로그래머", "사회선생님", "미용사", "간호사"]
+student = ["프로그래머", "사회선생님", "미용사", "수간호사", "고양이좋아"]
 print(student)
 student = [len(i) for i in student]
 print(student)
@@ -13,5 +13,5 @@ print(student)
 # 학생 이름을 대문자로 변환
 student = ["aaA", "bBb", "Ccc"]
 print(student)
-student = [i.upper() for i in student]
+student = [i.lower() for i in student]
 print(student)
