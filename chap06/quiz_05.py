3bb3d18ce27b377b9f5934e51e357da8f9c73500
didiha <hcring@naver.com>
2023-05-09 12:46:56 +0900

    20230509

--- chap06/quiz_05.py
+++ chap06/quiz_05.py
@@ -1,6 +1,6 @@
 from random import *
 count = 0
-for i in range(1, 51):
-    time = randrange(5, 51)
-    if 5 <= time <=15:
-       print("[O] {0}")
+for i in range(1, 51): # 1~50 승객 번호
+    time = randrange(5, 51) # 5~51분 소요 시간
+    if 5 <= time <=15: # 5~15분 이내의 손님, 탑승 승객 수 +1
+       print("[O] {0}번 째 손님 (소요시간: {1}분)".format(i, time))
