d9bea3eab58517babad5bcda265c54b6e897cee9
didiha <hcring@naver.com>
2023-05-13 21:10:17 +0900

    20230513

--- chap07/7-2.py
+++ chap07/7-2.py
@@ -6,6 +6,16 @@ def deposit(balance, money):
     print("입금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance + money))
     return balance + money
 
+def withDraw(balance, money):
+    if balance >= money:
+        print("출금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance - money))
+        return balance - money
+
+    else:
+        print("출금이 완료되지 않았습니다. 잔액은 {0}원 입니다.".format(balance))
+        return balance
+
 balance = 0 # 잔액
 balance = deposit(balance, 1000)
 print(balance)
+balance = withDraw(balance, 500)
