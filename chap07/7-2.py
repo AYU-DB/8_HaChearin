8463d09e8f246343e830ace36b81da02589ffda0
didiha <hcring@naver.com>
2023-05-13 21:15:03 +0900

    20230513

--- chap07/7-2.py
+++ chap07/7-2.py
@@ -14,8 +14,14 @@ def withDraw(balance, money):
     else:
         print("출금이 완료되지 않았습니다. 잔액은 {0}원 입니다.".format(balance))
         return balance
+    
+def withDraw_night(balance, money):
+    commission = 100 # 수수료 100원
+    return commission, withDraw(balance, money) - commission
 
 balance = 0 # 잔액
 balance = deposit(balance, 1000)
 print(balance)
-balance = withDraw(balance, 500)
+balance = withDraw(balance, 2000)
+commission, balance = withDraw_night(balance, 500)
+print("수수료 {0}원이며, 잔액은 {1}원 입니다.".format(commission, balance))
