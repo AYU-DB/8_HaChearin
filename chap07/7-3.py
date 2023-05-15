8bc53f1a516d725112bf2415cde1518b2aecd409
didiha <hcring@naver.com>
2023-05-15 14:18:20 +0900

    20230515

--- chap07/7-3.py
+++ chap07/7-3.py
@@ -1,5 +1,5 @@
 def profile(name, age, main_lang):
-    print("이름: {0}\ b: {1}\t주 사용 언어: {2}" \
+    print("이름: {0}\t: {1}\t주 사용 언어: {2}" \
           .format(name, age, main_lang))
     
 profile("a", 22, "C언어")
