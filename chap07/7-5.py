066480aa80f028a43315020c929beb522a989985
didiha <hcring@naver.com>
2023-05-21 19:43:54 +0900

    20230521

--- chap07/7-5.py
+++ chap07/7-5.py
@@ -1,2 +1,6 @@
 def profile(name, age, lang1, lang2, lang3, lang4, lang5):
     print("이름: {0}\t나이 : {1}\t" .format(name, age), end=" ")
+    print(lang1, lang2, lang3, lang4, lang5)
+
+profile("a", 21, "Python", "Java", "C", "C++", "C#")
+profile("b", 22, "Kotlin", "Swift", "", "", "")
