f6518575528bc53f42e5df19150cf5f8505b88cd
didiha <hcring@naver.com>
2023-05-22 16:38:21 +0900

    20230522

--- chap07/7-5.py
+++ chap07/7-5.py
@@ -1,6 +1,12 @@
-def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+'''def profile(name, age, lang1, lang2, lang3, lang4, lang5):
     print("이름: {0}\t나이 : {1}\t" .format(name, age), end=" ")
     print(lang1, lang2, lang3, lang4, lang5)
+    '''
+def profile(name, age, *language):
+    print("이름: {0}\t나이 : {1}\t" .format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
 
-profile("a", 21, "Python", "Java", "C", "C++", "C#")
-profile("b", 22, "Kotlin", "Swift", "", "", "")
+profile("a", 21, "Python", "Java", "C", "C++", "C#", "JavaScript")
+profile("b", 22, "Kotlin", "Swift")
