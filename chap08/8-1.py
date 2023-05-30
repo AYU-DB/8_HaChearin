31b1dfc5d9d28ea59dbd1e1c2dac766e7682828b
didiha <hcring@naver.com>
2023-05-31 01:06:51 +0900

    20230531

--- chap08/8-1.py
+++ chap08/8-1.py
@@ -4,3 +4,9 @@ print("Python" + "Java")
 print("Python", "Java", sep=", ")
 print("Python", "Java", "JavaScript", sep=" vs ")
 print("이 문장 마지막에는 물음표가 붙습니다", end = "?")
+
+# ljust(숫자) <<< 얘는 글자 뒤에 프린트 되는 공백 개수
+print("\n")
+scores = {"수학":0, "영어":50, "코딩":100}
+for subject, score in scores.items():
+    print(subject.ljust(3), score)
