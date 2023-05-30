d12bc47cabe77dd920733839c974559e39dff054
didiha <hcring@naver.com>
2023-05-31 00:54:13 +0900

    20230531

--- chap08/8-1.py
+++ chap08/8-1.py
@@ -1,3 +1,6 @@
+# 프린트 사이사이에 명령어 sep을 이용해서 단어 삽입하기
+
 print("Python" + "Java")
 print("Python", "Java", sep=", ")
 print("Python", "Java", "JavaScript", sep=" vs ")
+print("이 문장 마지막에는 물음표가 붙습니다", end = "?")
