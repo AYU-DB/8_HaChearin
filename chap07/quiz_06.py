db7d30cd251bec164cf2015019eda8a740c561a7
didiha <hcring@naver.com>
2023-05-30 14:39:40 +0900

    20230530

--- chap07/quiz_06.py
+++ chap07/quiz_06.py
@@ -1,9 +1,14 @@
 averange = 0
 def std_weight(height, gender):
     global averange
-    if gender == '남':
+    if gender == "남":
         averange = height * height * 22
-    if gender == '여':
+    if gender == "여":
         averange = height * height * 21
+    return averange
 
-print    
+height = 172
+gender = "여"
+weight = round(std_weight(height / 100, gender), 2)
+
+print("키: {0}\n성별: {1}\n평균 몸무게: {2}\n" .format(height, gender, weight))
