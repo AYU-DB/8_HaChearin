05ca336f8532a6629bd9e432b77c2dff1daa964a
didiha <hcring@naver.com>
2023-05-31 01:16:47 +0900

    20230531

--- chap08/8-1.py
+++ chap08/8-1.py
@@ -12,3 +12,7 @@ print("\n")
 scores = {"수학":0, "영어":50, "코딩":100}
 for subject, score in scores.items():
     print(subject, str(score).rjust(4))
+
+print("\n")
+for subject, score in scores.items():
+    print(subject.ljust(8), str(score))
