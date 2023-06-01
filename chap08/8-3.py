39fdaddcec4098949d371ab3a57a966972153f99
didiha <hcring@naver.com>
2023-06-02 02:24:57 +0900

    20230602

--- chap08/8-3.py
+++ chap08/8-3.py
@@ -27,5 +27,13 @@ while True:
     line = score_file.readline()
     if not line:
         break
-    print(line)
+    print(line, end = "")
+score_file.close()
+
+print("\n")
+
+score_file = open("score.txt", "r", encoding = "utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end = "")
 score_file.close()
