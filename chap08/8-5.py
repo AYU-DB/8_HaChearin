63999470dc009fe29a7e956daa70acba25d416ba
didiha <hcring@naver.com>
2023-06-06 12:06:43 +0900

    20230606

--- chap08/8-5.py
+++ chap08/8-5.py
@@ -1,4 +1,5 @@
-import pickle
+with open("study.txt", "w", encoding="utf-8") as study_file:
+    study_file.write("파이썬 하는 중")
 
-with open("profile.pickle", "rb") as profile_file:
-    print(pickle.load(profile_file))
+with open("study.txt", "r", encoding="utf-8") as study_file:
+    print(study_file.read())
