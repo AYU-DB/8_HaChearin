8038807958f49e09e053647459bce0679031a906
didiha <hcring@naver.com>
2023-06-28 00:36:29 +0900

    20230628

--- chap09/9-1.py
+++ chap09/9-1.py
@@ -15,9 +15,18 @@ tank_damage = 35
 print("{} 유닛이 생성되었습니다.".format(tank_name))
 print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
 
+# 탱크2: 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+tank2_name = "탱크2"
+tank2_hp = 150
+tank2_damage = 35
+
+print("{} 유닛이 생성되었습니다.".format(tank2_name))
+print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+
 def attack(name, location, damage):
     print("{0}: {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(
         name, location, damage))
     
 attack(name, "1시", damage)
 attack(tank_name, "2시", tank_damage)
+attack(tank2_name, "2시", tank2_damage)
