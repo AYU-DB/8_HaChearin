00f8cb3538277dddb7771c65e66433c5cc1ee287
didiha <hcring@naver.com>
2023-06-29 20:52:10 +0900

    20230629

--- chap09/9-3.py
+++ chap09/9-3.py
@@ -8,3 +8,11 @@ class Unit:
 
 wraith1 = Unit("레이스", 80, 5)
 print("유닛 이름: {0}, 공격력: {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤: 상대방 유닛을 내 것으로 만드는 것(빼앗음)
+
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
