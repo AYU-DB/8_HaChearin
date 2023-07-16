7b33f310eefd5b2a4aa592ea4f9465dac2432b0a
didiha <hcring@naver.com>
2023-07-16 15:45:18 +0900

    20230716

--- chap09/practice_calss.py
+++ chap09/practice_calss.py
@@ -6,7 +6,12 @@ class Flyable:
     def __init__(self):
         print("Flyable 생성자")
 
-class FlyableUnit(Unit, Flyable):
+class FlyableUnit(Flyable, Unit):
     def __init__(self):
-        super().__init__()
+        # super().__init__() // 앞에 있는 매개 변수(현 상태에서는 \\
+        # FlyableUnit 의 Flyable)만 실행됨.
+        Unit.__init__(self)
+        Flyable.__init__(self)
 
+# 드랍쉽
+dropship = FlyableUnit()
