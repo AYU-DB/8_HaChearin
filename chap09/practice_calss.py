50803df7b446c2e0457036558598d54be2436f36
didiha <hcring@naver.com>
2023-07-16 15:52:48 +0900

    20230716

--- chap09/practice_calss.py
+++ chap09/practice_calss.py
@@ -1,15 +1,16 @@
-class Unit:
+class Unit: # 부모 클래스
     def __init__(self):
         print("Unit 생성자")
 
-class Flyable:
+class Flyable: # 부모 클래스
     def __init__(self):
         print("Flyable 생성자")
 
-class FlyableUnit(Flyable, Unit):
+class FlyableUnit(Flyable, Unit): # 자식 클래스
     def __init__(self):
         # super().__init__() // 앞에 있는 매개 변수(현 상태에서는 \\
         # FlyableUnit 의 Flyable)만 실행됨.
+        # super()는 자식 클래스에서 부모 클래스를 접근할 수 있게 한다.
         Unit.__init__(self)
         Flyable.__init__(self)
 
